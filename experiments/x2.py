c64c88500ecd08110e9a428f1bbe08ea9b70c4f5
pakitowka <whitesaint12@gmail.com>
2023-03-30 15:57:12 +0200

    day2 capitalize every word

--- experiments/x2.py
+++ experiments/x2.py
@@ -1,11 +1,11 @@
-
+user_prompt = "Enter a todo:"
 
 
 todos = []
 
 
 while True:
-    user_prompt = "Enter a todo:"
+
     todo = input(user_prompt)
-    print(todo.capitalize())
+    print(todo.title())
     todos.append(todo)
