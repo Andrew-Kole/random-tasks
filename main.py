02599b730d94533cb3b34a8a3b0e44e05127a639
pakitowka <whitesaint12@gmail.com>
2023-03-30 15:33:57 +0200

    day2 loops

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 user_promt = "Enter to do:"
-todo1 = input(user_promt)
-todo2 = input(user_promt)
-todo3 = input(user_promt)
 
-todos = [todo1, todo2, todo3,"hello"]
-print(todos)
 
-print(type(todo1))
+
+while True:
+    todo = input(user_promt)
+    print(todo)
+    print("Next....")
+
