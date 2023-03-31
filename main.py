e81174e226a5cca769218850e26b8a1e035faef9
pakitowka <whitesaint12@gmail.com>
2023-03-31 18:59:54 +0200

    day3 match case

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-user_promt = "Enter to do:"
-
 todos = []
 
 while True:
-    todo = input(user_promt)
-    todos.append(todo.capitalize())
-    print(todos)
+    user_action = input("Type add, show, or exit: ")
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit' :
+            break
+
+print("bye")
 
 
