ccf061dd7f51b162fdf35db05768cdb1772b0932
pakitowka <whitesaint12@gmail.com>
2023-03-31 19:42:09 +0200

    day3 exercise1

---
+++ coding exercises/day3task1.py
@@ -0,0 +1,9 @@
+user_says = input("Your motherhood?")
+
+match user_says:
+    case "USA":
+        print("Hello")
+    case "India":
+        print("Namaste")
+    case "Germany":
+        print("Hallo")
