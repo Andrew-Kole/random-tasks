bc1f8fb1fd3ff8ae3d01a9370a3371ec1905284d
pakitowka <whitesaint12@gmail.com>
2023-03-31 19:47:16 +0200

    day3 exercise2

--- coding exercises/day3task1.py
+++ coding exercises/day3task1.py
@@ -1,3 +1,19 @@
+
+"""
+Create a program that does the following:
+
+1. Prompts the user to input the country they are from.
+
+2. If the user enters the word USA, the program prints out Hello.
+
+3. If the user enters the word  India, the program prints out Namaste.
+
+4. If the user enters the word Germany, the program prints out Hallo.
+
+Note: Strings are case-sensitive in Python, meaning germany and Germany are treated as two different strings.
+"""
+
+
 user_says = input("Your motherhood?")
 
 match user_says:
---
+++ coding exercises/day3task2.py
@@ -0,0 +1,16 @@
+"""
+ingredients = ["john smith", "sen plakay", "dora ngacely"]
+Copy-paste the above line into your IDE and write a for-loop below that line that makes the program produce the following output:
+
+
+
+
+Tip:  Use the str.title() method to convert strings to Title Case.
+"""
+
+
+
+ingredients = ["john smith", "sen plakay", "dora ngacely"]
+
+for name in ingredients:
+    print(name.title())
