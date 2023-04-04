9e1bbb4361216bb027e4086e1df36ab846877da5
pakitowka <whitesaint12@gmail.com>
2023-04-04 10:42:35 +0200

    day6  we have to close files to let other parts of app to interact it

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ while True:
 
             file = open("todos.txt", "r")
             todos = file.readlines()
+            file.close()
 
             todos.append(todo)
 
             file = open('todos.txt', 'w')
             file.writelines(todos)
+            file.close()
         case 'show' | 'display':
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
