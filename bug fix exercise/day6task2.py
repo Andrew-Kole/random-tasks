f58d26adf28f4f89e3f58e5b5165f7a699b166a6
pakitowka <whitesaint12@gmail.com>
2023-04-04 12:15:51 +0200

    day6  bug fix exercise2 + cosmetic changes

---
+++ bug fix exercise/day6task2.py
@@ -0,0 +1,9 @@
+"""
+ The code below tries to write the string "100.2" to the text file. However, there is an error. Try to fix the error.
+
+file = open("data.txt", 'r')
+file.write("100.12")
+"""
+
+file = open("../files/data1.txt", 'w')
+file.write("100.12")
