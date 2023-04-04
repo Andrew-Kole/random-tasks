a4212d7e73491e381a27d8a83f857c684b95acef
pakitowka <whitesaint12@gmail.com>
2023-04-04 11:49:49 +0200

    day6  exercise 2 reading file and counting characters

---
+++ coding exercises/day6task2.py
@@ -0,0 +1,6 @@
+"""
+Write a program that reads the essay.txt file and returns the number of characters contained in the file.
+"""
+
+file = open("../files/essay.txt", "r")
+print(len(file.readline()))
