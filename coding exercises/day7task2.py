5484b3aa99a1a6ba968e9d17b1eb35d0c0c0f000
pakitowka <whitesaint12@gmail.com>
2023-04-06 11:27:32 +0200

    day7  exercise2

---
+++ coding exercises/day7task2.py
@@ -0,0 +1,15 @@
+"""
+usernames = ["john 1990", "alberta1970", "magnola2000"]
+
+Extend the code above so the code prints out a list containing the number of characters for each username.
+
+The output of your code should be as below:
+
+[9, 11, 11]
+"""
+
+usernames = ["john 1990", "alberta1970", "magnola2000"]
+
+printusernames_char_quantity = [len(username) for username in usernames]
+
+print(printusernames_char_quantity)
