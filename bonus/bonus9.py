425ad80a83a8ccadd18dc287999504dbfdcf2189
pakitowka <whitesaint12@gmail.com>
2023-04-08 20:52:07 +0200

    day9 dictionaries usage

--- bonus/bonus9.py
+++ bonus/bonus9.py
@@ -1,17 +1,17 @@
 password = input("Enter new password: ")
-result = []
+result = {}
 
 if len(password) >= 8:
-    result.append(True)
+    result["length"] = True
 else:
-    result.append(False)
+    result["length"] = False
 
 digit = False
 for i in password:
     if i.isdigit():
         digit = True
 
-result.append(digit)
+result["digits"] = digit
 
 uppercase = False
 
@@ -19,9 +19,9 @@ for i in password:
     if i.isupper():
         uppercase = True
 
-result.append(uppercase)
+result["upper-case"] = uppercase
 
-if all(result):
+if all(result.values()):
     print("Strong password.")
 else:
     print("Weak password.")
