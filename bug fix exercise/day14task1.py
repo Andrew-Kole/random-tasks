27791c73abed848becceb7926bd80dfa15c0d351
pakitowka <whitesaint12@gmail.com>
2023-04-16 15:09:44 +0200

    day14 bug fix exercise1

---
+++ bug fix exercise/day14task1.py
@@ -0,0 +1,22 @@
+"""
+Bug-Fixing Exercise 1
+The program depicted below consists of two Python files. The program tries to count and print out the number of periods in the "Trees are good. Grass is green." . However, running the main.py file returns an error. Please fix the error.
+
+main.py:
+
+import functions
+
+nr_of_periods = count("Trees are good. Grass is green.")
+print(nr_of_periods)
+
+
+functions.py:
+
+def count(phrase):
+    return phrase.count('.')
+"""
+from modules import bfday14task1func
+
+nr_of_periods = bfday14task1func.count("Trees are good. Grass is green.")
+print(nr_of_periods)
+
