a157a0ef1a34e73fa38b02fa9daaf6e9c36444a9
pakitowka <whitesaint12@gmail.com>
2023-04-16 11:19:09 +0200

    day14 modules

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-def get_todos(filepath="files/todos.txt"):
-    """Read a text file and return a list
-    of to-do items.
-    """
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="files/todos.txt"):
-    """Write the to-do items list in the text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+#from functions import get_todos, write_todos
+import functions
 
 
 while True:
@@ -21,15 +10,15 @@ while True:
     if user_action.startswith("add") or user_action.startswith("new"):
         todo = user_action[4:] + "\n"
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + "\n")
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith("show") or user_action.startswith("display"):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         # new_todos = [item.strip(("\n") for item in todos]
 
@@ -42,13 +31,13 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
 
             new_item = input("Enter new todo: ")
             todos[number] = new_item + "\n"
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -58,11 +47,11 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             todos.pop(number - 1)
 
-            write_todos(todos)
+            functions.write_todos(todos)
         except IndexError:
             print("There is no item with that number")
             continue
