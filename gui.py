94f88cc025a1b1bacd3d046bd4318c5f608a4fe3
pakitowka <whitesaint12@gmail.com>
2023-04-17 20:04:24 +0200

    day16 zip comressor gui

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter to-do")
 add_button = sg.Button("Add")
 
-window = sg.Window("My to-do app", layout=[label, input_box, add_button])
+window = sg.Window("My to-do app", layout=[[label], [input_box, add_button]])
 window.read()
 window.close()
